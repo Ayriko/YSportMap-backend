f22546c9c5317f630d9b55ffbb1ae3fdc2ec5808
amoiska <aymeric.moiska@ynov.com>
2024-08-31 20:26:36 +0200

    added simple documentation

--- src/api.ts
+++ src/api.ts
@@ -8,13 +8,15 @@ app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
-// Healthcheck endpoint
+// Healthcheck endpoint to verify if the server is running
 app.get('/', (req, res) => {
   res.status(200).send({ status: 'ok' });
 });
 
 const api = express.Router();
 
+// We will use this type to define the filters that can be applied to the equipments endpoint
+// The one named are treated differently than the others in the final API request
 type Filters = {
   global_search?: string | undefined;
   equip_type_name?: string | undefined;
@@ -22,6 +24,7 @@ type Filters = {
   [key: string]: string | undefined;
 };
 
+// Endpoint to get a list of equipments based on filters
 api.get('/equipments', async (req: Request, res: Response) => {
   const filters: Filters = req.query as unknown as Filters;
   const limit = parseInt(req.query.limit as string) || 10;
@@ -30,33 +33,39 @@ api.get('/equipments', async (req: Request, res: Response) => {
   let finalUrl = `https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?limit=${limit}&offset=${offset}&timezone=UTC&include_links=false&include_app_metas=false`;
   let whereClauses: string[] = [];
 
+  // Add global search filter if provided
   if (filters.global_search && filters.global_search.trim() !== '') {
     const globalSearch = filters.global_search.trim();
     whereClauses.push(`suggest(equip_nom,inst_nom,inst_com_nom,"${globalSearch}")`);
   }
 
+  // Add equipment type name filter if provided
   if (filters.equip_type_name && filters.equip_type_name.trim() !== '') {
     const equip_type_name = filters.equip_type_name.trim();
     whereClauses.push(`suggest(equip_type_name,"${equip_type_name}")`);
   }
 
+  // Add user location filter if provided
   if (filters.user_location && filters.user_location.trim() !== '') {
     const [userLat, userLon] = filters.user_location.split(',').map(coord => parseFloat(coord.trim()));
     whereClauses.push(`within_distance(coordonnees, geom'POINT(${userLat} ${userLon})', 10km)`);
   }
 
+  // Add any other filters provided
   Object.entries(filters)
     .filter(([key, value]) => key !== 'global_search' && key !== 'equip_type_name' && key !== 'user_location' && key !== 'limit' && key !== 'offset' && value && value.trim() !== '')
     .forEach(([key, value]) => {
       whereClauses.push(`${key}="${value?.trim()}"`);
     });
 
+  // Append where clauses to the final URL
   if (whereClauses.length > 0) {
     const whereQuery = whereClauses.join('&where=');
     finalUrl += `&where=${whereQuery}`;
   }
 
   try {
+    // Fetch data from the external API
     const response = await fetch(finalUrl);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -69,11 +78,13 @@ api.get('/equipments', async (req: Request, res: Response) => {
   }
 });
 
+// Endpoint to get details of specific equipment by its number
 api.get('/equipments/:equip_numero', async (req: Request, res: Response) => {
   const equip_numero = req.params.equip_numero;
   const equipmentUrl = `https://equipements.sports.gouv.fr/api/explore/v2.1/catalog/datasets/data-es/records?timezone=UTC&include_links=false&include_app_metas=false&where=equip_numero="${equip_numero}"`;
 
   try {
+    // Fetch data from the external API
     const response = await fetch(equipmentUrl, {
       method: 'GET',
       headers: {
@@ -93,6 +104,5 @@ api.get('/equipments/:equip_numero', async (req: Request, res: Response) => {
   }
 });
 
-
-// Version the api
+// Version the API
 app.use('/api/v1', api);
